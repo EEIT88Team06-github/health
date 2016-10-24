e4a8deb65aa063ffb7e16fa44f8c4f6c81bcfe6a
劉蒨蓉 <eeit8819@III-ITC>
2016-10-24 19:57:16 +0800

    .

--- src/main/java/org/iiiedu/eeit88/health/calories/controller/ModifyFoodQtyServlet.java
+++ src/main/java/org/iiiedu/eeit88/health/calories/controller/ModifyFoodQtyServlet.java
@@ -9,8 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.iiiedu.eeit88.health.bean.MemberBean;
 import org.iiiedu.eeit88.health.calories.service.AbsorbCart;
-import org.iiiedu.eeit88.health.member.model.MemberBean;
+
 
 
 
